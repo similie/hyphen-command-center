1a81df192e6eae07ed7c6fadce91d9701ad20f4a
Adam Smith <adam.smith@similie.org>
2025-11-28 18:20:37 +0800

    chore: Shai-Hulud 2.0 security scanning

--- src/routes/api/v1/config/site/+server.ts
+++ src/routes/api/v1/config/site/+server.ts
@@ -1,29 +1,24 @@
 import { createCacheKey } from "$lib";
 import { CacheKeys } from "$lib";
-import { type PutQueryModel, UserRoles, type SiteConfig } from "$lib";
-import { SendJson, RedisCache } from "$lib/server";
+import { type PutQueryModel, type SiteConfig } from "$lib";
+import { RedisCache } from "$lib/server";
 import { ConfigQuery, UserQuery } from "$lib/server/db/query";
-import { error, type RequestHandler } from "@sveltejs/kit";
+import { error, json, type RequestHandler } from "@sveltejs/kit";
 
 export const PUT: RequestHandler = async (event) => {
-  try {
-    const session = await UserQuery.isAdmin(event);
-    if (!session) {
-      throw error(401, "Unauthorized");
-    }
-    const body = (await event.request.json()) as PutQueryModel<SiteConfig>;
-
-    const query = new ConfigQuery();
-    const configs = await query.update(body.values, body.where);
-
-    for (const config of configs) {
-      const key = createCacheKey(CacheKeys.SITE_CONFIG, config.key);
-      await RedisCache.set(key, config);
-    }
-
-    return SendJson(configs);
-  } catch (e: any) {
-    console.error("Conversation Error", e);
-    throw error(500, e.message);
+  const session = await UserQuery.isAdmin(event);
+  if (!session) {
+    throw error(401, "Unauthorized");
   }
+  const body = (await event.request.json()) as PutQueryModel<SiteConfig>;
+
+  const query = new ConfigQuery();
+  const configs = await query.update(body.values, body.where);
+
+  for (const config of configs) {
+    const key = createCacheKey(CacheKeys.SITE_CONFIG, config.key);
+    await RedisCache.set(key, config);
+  }
+
+  return json(configs);
 };
